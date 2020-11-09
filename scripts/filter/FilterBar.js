1a3ff99f8e499e8044bde1f451cd8bb6a474a400
Jasmin Kaset <kasetski@gmail.com>
2020-11-09 15:26:55 -0600

    started filter modules

---
+++ scripts/filter/FilterBar.js
@@ -0,0 +1,13 @@
+import {MoodFilter} from "./MoodFilter.js"
+
+const contentTarget = document.querySelector(".filters"
+)
+
+const FilterBar = () = {
+  render = () = {
+    contentTarget.innerHTML = `
+      ${MoodFilter()}
+    `
+  }
+  render()
+}
