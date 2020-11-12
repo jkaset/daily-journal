622bc13e1b0a94c769e94a7002c78219e047d694
Jasmin Kaset <kasetski@gmail.com>
2020-11-12 13:27:54 -0600

    took out + in moodfilter.js

--- scripts/filter/MoodFilter.js
+++ scripts/filter/MoodFilter.js
@@ -4,14 +4,14 @@ import {MoodButton} from './Mood.js'
 
 
 export const MoodFilter = () => {
-    getMoods().then(() => {
+   getMoods().then(() => {
       const moodContainer = document.querySelector(".filters")
       const moodArray = useMoods()
       let buildMoodList = ""
       for (const mood of moodArray) {
         buildMoodList += MoodButton(mood)
       }
-      moodContainer.innerHTML += `<div class="filterBox">
-      <ul class="moodF">${buildMoodList}<ul></div>`
+       moodContainer.innerHTML = `<div class="filterBox">
+       <ul class="moodF">${buildMoodList}<ul></div>`
     })
   }
